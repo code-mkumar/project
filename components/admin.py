e05543d6d9e58887d8f9b3336dff6e662cb08182
code-mkumar <codehunting2k24@gmail.com>
2025-01-04 16:49:17 +0530

    Update admin.py

--- components/admin.py
+++ components/admin.py
@@ -104,7 +104,7 @@ def admin_page():
             with open(file_path, "r") as f:
                 existing_content = f.read()
             edited_existing_content = st.text_area("Edit Existing File Content", value=existing_content, height=300,disabled=True)
-       if st.checkbox("update"):
+        if st.checkbox("update"):
             edited_existing_content = st.text_area("Edit Existing File Content", value=existing_content, height=300)
             if st.button("update Content",key="update"):
                 file_path = os.path.join(folder_path, existing_file)
