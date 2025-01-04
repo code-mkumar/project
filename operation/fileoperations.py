f6e1e3ae41ad9e4fe51dd3ebade60952a5d2e0dd
code-mkumar <codehunting2k24@gmail.com>
2025-01-04 09:13:20 +0530

    Update fileoperations.py

--- operation/fileoperations.py
+++ operation/fileoperations.py
@@ -1,30 +1,30 @@
-def read_student_files():
-    with open("student_role.txt", "r") as role_file:
-        role_content = role_file.read()
-    with open("student_sql.txt", "r") as sql_file:
-        sql_content = sql_file.read()
-    return role_content, sql_content
+# def read_student_files():
+#     with open("student_role.txt", "r") as role_file:
+#         role_content = role_file.read()
+#     with open("student_sql.txt", "r") as sql_file:
+#         sql_content = sql_file.read()
+#     return role_content, sql_content
 
-def read_default_files():
-    with open("default.txt", "r") as role_file:
-        role_content = role_file.read()
-    with open("default_sql.txt", "r") as sql_file:
-        sql_content = sql_file.read()
-    return role_content, sql_content
+# def read_default_files():
+#     with open("default.txt", "r") as role_file:
+#         role_content = role_file.read()
+#     with open("default_sql.txt", "r") as sql_file:
+#         sql_content = sql_file.read()
+#     return role_content, sql_content
 
-def read_staff_files():
-    with open("staff_role.txt", "r") as role_file:
-        role_content = role_file.read()
-    with open("staff_sql.txt", "r") as sql_file:
-        sql_content = sql_file.read()
-    return role_content, sql_content
+# def read_staff_files():
+#     with open("staff_role.txt", "r") as role_file:
+#         role_content = role_file.read()
+#     with open("staff_sql.txt", "r") as sql_file:
+#         sql_content = sql_file.read()
+#     return role_content, sql_content
 
-def read_admin_files():
-    with open("admin_role.txt", "r") as role_file:
-        role_content = role_file.read()
-    with open("admin_sql.txt", "r") as sql_file:
-        sql_content = sql_file.read()
-    return role_content, sql_content
+# def read_admin_files():
+#     with open("admin_role.txt", "r") as role_file:
+#         role_content = role_file.read()
+#     with open("admin_sql.txt", "r") as sql_file:
+#         sql_content = sql_file.read()
+#     return role_content, sql_content
 
 def write_to_file(filename, data=''):
     """
