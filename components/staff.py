2214da4833887f129f2fa6b8a08a43afb9edd260
mukesh <22us25@anjaconline.org>
2025-01-06 09:47:06 +0530

    1/6/25 commit

--- components/staff.py
+++ components/staff.py
@@ -148,7 +148,7 @@ def staff_page():
             st.chat_message("human").text(question)
             combined_prompt = operation.preprocessing.create_combined_prompt(question, sql_content)
             response = genai.gemini.get_gemini_response(combined_prompt,data)
-            print(response)
+            st.write(response)
             # Display the SQL query
             # st.write("Generated SQL Query:", response)
             raw_query = response
